d8d0c2c537cac75d5be9246d77d2c6245805c764
lucilletarpi <98407181+lucilletarpi@users.noreply.github.com>
2023-07-03 11:13:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const startTimer = document.querySelector(".start").addEventListener("click", StartTime);
+const StopTimer =document.querySelector(".stop").addEventListener("click", StopTime);
+const ResetTimer =document.querySelector(".reset").addEventListener("click", ResetTime);
+console.log(startTimer);
+
+
+
+function StartTime(){
+console.log('start');
+
+};
+
+function StopTime(){
+    console.log('Stop');
+};
+
+function ResetTime(){
+    console.log('Reset');
+};
